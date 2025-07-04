744ee32e38e46296c461a944b8d0aceed8fc8d73
Victoria Wenning <victoria.wenning@yahoo.com>
2025-07-03 21:51:16 -0700

    week 11

--- src/js/templates.mjs
+++ src/js/templates.mjs
@@ -62,7 +62,7 @@ export function alertTemplate(alert) {
 
 export function visitorCenterTemplate(center) {
   return `<li class="visitor-center">
-  <h4>${center.name}</h4>
+  <h4><a href="visitor-center.html?id=${center.id}">${center.name}</a></h4>
   <p>${center.description}</p>
   <p>${center.directionsInfo}</p>
   </li>`;
@@ -70,4 +70,80 @@ export function visitorCenterTemplate(center) {
 
 export function activityListTemplate(activities) {
   return activities.map((activity) => `<li>${activity.name}</li>`).join("");
+}
+
+export function iconTemplate(iconId) {
+  return `<svg class="icon" role="presentation" focusable="false">
+            <use
+              xmlns:xlink="http://www.w3.org/1999/xlink"
+              xlink:href="/images/sprite.symbol.svg#${iconId}"
+            ></use>
+          </svg>`;
+}
+
+export function vcDetailsTemplate(elementId, summaryText, iconId, content) {
+  return `<details name="vc-details" id="${elementId}">
+          <summary>
+            ${iconTemplate(iconId)}
+            ${summaryText}
+          </summary>
+          ${content}
+        </details>`;
+}
+
+export function vcTitleTemplate(text) {
+  return `${iconTemplate("ranger-station")} ${text}`;
+}
+
+export function vcInfoTemplate(data) {
+  const image = data.images[0];
+  return `<figure>
+          <img src="${image.url}" alt="${image.altText}" />
+          <figcaption>${image.caption} <span>${image.credit}</span></figcaption>
+        </figure>
+        <p>${data.description}</p>`;
+}
+export function listTemplate(data, contentTemplate) {
+  const html = data.map(contentTemplate);
+  return `<ul>${html.join("")}</ul>`;
+}
+
+function vcAddressTemplate(data) {
+  return `<section>
+            <h3>${data.type} Address</h3>
+            <address>
+              ${data.line1}<br />
+              ${data.city}, ${data.stateCode} ${data.postalCode}
+            </address>
+          </section>`;
+}
+
+export function vcAddressesListTemplate(data) {
+  const physical = data.find((address) => address.type === "Physical");
+  const mailing = data.find((address) => address.type === "Mailing");
+  let html = vcAddressTemplate(physical);
+  if (mailing) {
+    html += vcAddressTemplate(mailing);
+  }
+  return html;
+}
+export function vcAmenityTemplate(data) {
+  return `<li>${data}</li>`;
+}
+export function vcDirectionsTemplate(data) {
+  return `<p>${data}</p>`;
+}
+export function vcContactsTemplate(data) {
+  return `<section class="vc-contact__email">
+            <h3>Email Address</h3>
+            <a href="email:${data.emailAddresses[0].emailAddress}">Send this visitor center an email</a>
+          </section>
+          <section class="vc-contact__phone">
+            <h3>Phone numbers</h3>
+            <a href="tel:+1${data.phoneNumbers[0].phoneNumber}">${data.phoneNumbers[0].phoneNumber}</a>
+          </section>`;
+}
+
+export function vcImageTemplate(data) {
+  return `<li><img src="${data.url}" alt="${data.altText}" ><li>`;
 }
---
+++ src/js/visitor-center.js
@@ -0,0 +1,82 @@
+import "../css/style.css"; // we can do this because we are using Vite...
+import "../css/visitor-center.css";
+import setHeaderFooter from "./setHeaderFooter.mjs";
+import { getParkData, getParkVisitorCenterDetails } from "./parkService.mjs";
+import {
+  vcInfoTemplate,
+  vcTitleTemplate,
+  vcDetailsTemplate,
+  vcAmenityTemplate,
+  vcImageTemplate,
+  listTemplate,
+  vcAddressesListTemplate,
+  vcDirectionsTemplate,
+  vcContactsTemplate
+} from "./templates.mjs";
+
+function getParam(param) {
+  const search = location.search;
+  const params = new URLSearchParams(search);
+  return params.get(param);
+}
+
+function buildPage(data) {
+  // since we have all the structure in place in the HTML, this solution chooses to insert content into the existing structure instead of generating all the structure with the content
+  document.querySelector(".vc-name").innerHTML = vcTitleTemplate(data.name);
+  document.querySelector(".vc-info").innerHTML = vcInfoTemplate(data);
+  const detailsEl = document.querySelector(".vc-details-list");
+  detailsEl.innerHTML = "";
+  // addresses section
+  const addressHTML = vcAddressesListTemplate(data.addresses);
+  detailsEl.insertAdjacentHTML(
+    "beforeend",
+    vcDetailsTemplate(
+      "vcAddresses",
+      "Addresses",
+      "heading-icon_map-pin",
+      addressHTML
+    )
+  );
+  // directions
+  detailsEl.insertAdjacentHTML(
+    "beforeend",
+    vcDetailsTemplate(
+      "vcDirections",
+      "Directions",
+      "directions",
+      vcDirectionsTemplate(data.directionsInfo)
+    )
+  );
+  // amenities section.
+  const amenitiesHTML = listTemplate(data.amenities, vcAmenityTemplate);
+  detailsEl.insertAdjacentHTML(
+    "beforeend",
+    vcDetailsTemplate(
+      "vcAmenities",
+      "Amenities",
+      "heading-icon_info",
+      amenitiesHTML
+    )
+  );
+  // contact section
+  const contactHTML = vcContactsTemplate(data.contacts);
+  detailsEl.insertAdjacentHTML(
+    "beforeend",
+    vcDetailsTemplate("vcContacts", "Contacts", "phone", contactHTML)
+  );
+  // gallery section
+  const galleryHTML = listTemplate(data.images, vcImageTemplate);
+  document
+    .querySelector(".vc-gallery")
+    .insertAdjacentHTML("beforeend", galleryHTML);
+}
+
+async function init() {
+  const parkData = await getParkData();
+  const id = getParam("id");
+  const centerDetails = await getParkVisitorCenterDetails(id);
+  setHeaderFooter(parkData);
+  buildPage(centerDetails);
+}
+
+init();
